bc4f8153a1e3f2685d965de42c263c0d80e0b6f9
Leonardo Ribera <lr21k@fsu.edu>
2026-04-22 12:34:48 -0400

    closes #8, created admin.py

--- project_djangodatawebapp/core/admin.py
+++ project_djangodatawebapp/core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import City, DataRun, WeatherRecord
 
-# Register your models here.
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "latitude", "longitude")
+    search_fields = ("name",)
+
+
+@admin.register(DataRun)
+class DataRunAdmin(admin.ModelAdmin):
+    list_display = ("source", "created_at")
+    list_filter = ("source",)
+
+
+@admin.register(WeatherRecord)
+class WeatherRecordAdmin(admin.ModelAdmin):
+    list_display = ("city", "record_time", "temperature_c", "windspeed_kmh", "source")
+    list_filter = ("city", "source")
+    search_fields = ("city__name",)
